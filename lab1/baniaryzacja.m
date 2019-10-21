8f97667c24245932576b171f7167e79fd17b924d
Lukasz Bil <lukasz_bil@wp.pl>
2019-10-21 09:45:53 +0200

    Rozpoczęcie lab2 i dokończenie zadania 5 z lab1

--- lab1/baniaryzacja.m
+++ lab1/baniaryzacja.m
@@ -1,38 +1,81 @@
 clear all;
+image = imread("Koala.jpg");
+ 
+ 
+image_nois = imnoise(image, 'salt & pepper'); %zaszumienie obrazka zad 4
+img_gray_nois = rgb2gray(image_nois);
+%imshow(image);
+ 
+% to gray
+img_gray = rgb2gray(image);
+img_hist = imhist(img_gray);
+%bar(img_hist);
+%imshow(img_gray);
+ 
+% binaryzacja
+TW = 116; %próg binaryzacji
+img_bin = img_gray > TW;
+%imshow(img_bin);
+ 
+%% OTSU
+% Zadanie 1.1
+ 
+ 
+% wyznaczanie progu binaryzacji metoda Otsu
+th = graythresh(img_gray)*255;
 
-image1 = imread('Koala.jpg');
-image2 = rgb2gray(image1);
+ 
+%% Palety barw
+% Zadanie 1.2
+% colormap
+ 
+img_cvt = cv.cvtColor(image, 'RGB2XYZ'); %zmiana palety barw
+Y = img_cvt(:, :, 2); %wybór palety jasnoœci
+img_cvt_th = graythresh(Y)*255; %otsu obrazu
+img_cvt_bin = Y > img_cvt_th; %binaryzacja
+ 
+ 
+img_xor = bitxor(img_cvt_bin, img_bin); %xorowanie macierzy w celu znalezienia ró¿nic
+imshow(img_xor);
+ 
+diffCount = sum(img_xor, 'all'); %suma elementów
 
-cv_image = cv.cvtColor(image1,'RGB2XYZ');%zmianan palety barw na ciexyz, imshow nie wyswietli tego bo nie jest rgb
-luminancja=cv_image(:,:,2);%wyci¹gamy luminancje
-th_cv=graythresh(luminancja)*255;%matoda otsu
+ 
+ 
+%% Propagowanie adaptacyjne
+% Zadanie 1.3
+%
+% progowanie funkcja cv.adaptiveThreshold
+ 
+img_adapt_mean = cv.adaptiveThreshold(img_gray, 'BlockSize', 11);
+imshow(img_adapt_mean);
+% figure(2);
+img_adapt_gauss = cv.adaptiveThreshold(img_gray, 'Method', 'Gaussian', 'BlockSize', 11);
+imshow(img_adapt_gauss);
 
-th=116;%próg binaryzacji 
-% th=graythresh(image2)*255;%matoda otsu
+%% Binaryzacja obrazu zaszumionego
+% Metoda Otsu
 
-image3 = image2 > th; %binaryzacja przez skalar
-image_cv = image2 > th_cv; %binaryzacja przez skalar
-histogram=imhist(image2);%histogram
+TW = 116; %próg binaryzacji
+img_bin_nois = img_gray_nois > TW;
+%imshow(img_bin_nois);
+ 
+%% Implementacja innych metod binaryzacji
+%Zadanie 1.5
+ 
+imshow(sauvola(img_gray,[100 100]))
+% figure(3);
+imshow(niblack(img_gray))
 
-image_xor=bitxor(image3,image_cv);
+%% Wynik
+% Metody Sauvola, Niblacka, Otsu
+figure
+subplot(131)
+imshow(sauvola(img_gray,[100 100]))
+subplot(132)
+imshow(niblack(img_gray))
+subplot(133)
+imshow(img_bin)
 
-suma=sum(image_xor,'all');
 
-% bar(histogram);szukamy siod³a, próg binaryzacji, dolina miedzy maksimami
-% figure();
-adaptacyjnaMean = cv.adaptiveThreshold(luminancja,'BlockSize',13);
-adaptacyjnaGaussian = cv.adaptiveThreshold(luminancja,'Method','Gaussian','BlockSize',11);
 
-img_noise=imnoise(image2,'salt & pepper');
-binaryzacja_zaszumionego= cv.adaptiveThreshold(img_noise,'BlockSize',11);
-
-image_xor_cv=bitxor(binaryzacja_zaszumionego,image2);
-
-suma_roznic_cv=sum(image_xor_cv,'all');
-imshow(binaryzacja_zaszumionego);
-% imshow(adaptacyjnaMean);
-% figure();
-% imshow(adaptacyjnaGaussian);
-% imshow(image3);
-% figure();
-% imshow(image_xor);
