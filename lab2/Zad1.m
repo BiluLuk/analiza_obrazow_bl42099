5cb218083be3414b74d85de92ed75513f1ae4669
Lukasz Bil <lukasz_bil@wp.pl>
2019-10-28 09:29:44 +0100

    Lab2 palety barw i inne metody bbinaryzacji

--- lab2/Zad1.m
+++ lab2/Zad1.m
@@ -1,16 +1,33 @@
 clear all;
 image1 = imread("H01.png");
+%%wczytywanie obrazka "tif" 
 
 [X, map] = imread('H01_GT.tif');
 imshow(X,map);
+%% Binaryzacja OTSU
+%Metoda GREY
 
 figure;
-imshow(binOtsu(image1));
+imshow(binOtsu(image1,'GREY'));
 
 
-function out = binOtsu(image)
-img_cvt = cv.cvtColor(image, 'RGB2GRAY');
+
+
+function out = binaryzacja(image,method,paleta)
+if(method == "OTSU")
+    if(paleta == "GREY")
+        figure;
+        imshow(binOtsu(cv.cvtColor(image1, 'RGB2GRAY')));
+    end
+end
+
+end
+
+function out = binOtsu(image,methoda)
+img_cvt = cv.cvtColor(image, methoda);
+img_cvt(:,:,2);
 th = graythresh(img_cvt)*255;
-img_bin = img_cvt > th;
+img_bin = image > th;
 out = img_bin;
 end
+
